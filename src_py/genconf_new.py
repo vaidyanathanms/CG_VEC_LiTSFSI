f821be5f8c77260951be80c114313aee9ae0be5e
vaidyaanthanms <mathamangal1@ornl.gov>
2024-10-08 11:18:44 -0400

    slight changes

--- src_py/genconf_new.py
+++ src_py/genconf_new.py
@@ -33,7 +33,7 @@ num_cores = 36 # Number of cores per node
 hpc_sys   = 'cades'  # Opt: kestrel, cades
 
 #---------input details - Topology--------------------------------
-frac_anions  = [1/5]#,1/10,1/15,1/20]#,1/20,1/10,1/6,1/5,1/3] # fraction of anions
+frac_anions  = [1/10,1/15,1/20]#,1/20,1/10,1/6,1/5,1/3] # fraction of anions
 tot_mons     = 6000 # total number of MONOMERS in the poly CHAIN
 chain_mw     = [60,40]#,60,90] # of monomer range per chain
 num_chains   = [int(tot_mons/x) for x in chain_mw] # of polymerized ch
@@ -97,9 +97,9 @@ elif hpc_sys == 'cades':
 else:
     raise RuntimeError('Unknown HPC system ' + hpc_sys)
     
-src_f90    = home_path + '/all_codes/CG_VEC_LiTSFSI/src_f90' #src_f90 dir
-src_lmp    = home_path + '/all_codes/CG_VEC_LiTSFSI/src_lmp' #src_lmp dir
-src_tcl    = home_path + '/all_codes/CG_VEC_LiTSFSI/src_tcl' #src_tcl dir
+src_f90    = home_path + '/all_codes/files_CG-SIC/src_f90' #f90 dir
+src_lmp    = home_path + '/all_codes/files_CG-SIC/src_lmp' #lmp dir
+src_lmp    = home_path + '/all_codes/files_CG-SIC/src_tcl' #tcl dir
 scratchdir = scr_path  + '/cg_sic' #output headdir
 scr_head   = 'sic_mixedvec_listsfi' # head dir scratch'
 
