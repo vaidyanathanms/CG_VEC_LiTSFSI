5f4f14bcacbde590817fa4753b137f69e6321c18
vms_laptop <vm5@ornl.gov>
2025-03-05 14:41:56 -0500

    added username python plotting files - initial commit

---
+++ src_matpy/aux_plot.py
@@ -0,0 +1,308 @@
+# Auxiliary data for generic plot
+#------------------------------------------------------------------
+# Make labels and graphics
+# Version: Mar-04-2025
+#------------------------------------------------------------------
+
+# Import modules
+import os
+import sys
+import numpy
+import re
+import shutil
+import glob
+import math
+import fileinput
+import subprocess
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+#------------------------------------------------------------------
+
+# General copy script
+def gencpy(dum_maindir,dum_destdir,fylname):
+
+    srcfyl = dum_maindir + '/' + fylname
+
+    if not os.path.exists(srcfyl):
+        print('ERROR: ', srcfyl, 'not found')
+        return
+
+    desfyl = dum_destdir + '/' + fylname
+    shutil.copy2(srcfyl,desfyl)
+#------------------------------------------------------------------
+
+def ret_temp_dir(scr_dir,inp_type,biomass,dispval,casenum,subdir,\
+                 solv_type='None'):
+    # Check all directories
+    head_dir = scr_dir + '/' + inp_type
+    if not os.path.isdir(head_dir):
+        raise RuntimeError(head_dir + " does not exist!")
+       
+    poly_dir = head_dir + '/' + biomass
+    if not os.path.isdir(poly_dir):
+        raise RuntimeError(poly_dir + " does not exist!")
+        
+    if inp_type == 'melts':
+        fig_dir  = poly_dir + '/results_figs'
+        if not os.path.isdir(fig_dir):
+            os.mkdir(fig_dir)
+
+        if dispval == 'expts':
+            poly_dir = poly_dir + '/expts'
+        else:
+            poly_dir = poly_dir + '/pdi_' + str(dispval)
+        if not os.path.isdir(poly_dir):
+            raise RuntimeError(poly_dir + " does not exist!")
+
+    rundir = poly_dir + '/run_' + str(casenum)
+    if not os.path.isdir(rundir):
+        raise RuntimeError(rundir + " does not exist!")
+
+    if inp_type == 'solvents':
+        workdir1 = rundir + '/' + solv_type
+    elif inp_type == 'cosolvents':
+        h_workdir = rundir + '/' + solv_type
+        workdir1 = h_workdir + '/' + solv_type + '_water'
+    else:
+        workdir1 = rundir #even for inp_type = melts
+
+    if not os.path.isdir(workdir1):
+        raise RuntimeError(workdir1, " does not exist!")
+
+    anadir = workdir1 + '/T_' + str(subdir)
+    if not os.path.isdir(anadir):
+        raise RuntimeError(anadir, " does not exist!")
+
+    return workdir1, anadir, fig_dir
+#------------------------------------------------------------------
+
+def ret_ana_dir(scr_dir,inp_type,biomass,dispval,casenum,subdir,\
+                 solv_type='None'):
+    # Check all directories
+    head_dir = scr_dir + '/' + inp_type
+    if not os.path.isdir(head_dir):
+        raise RuntimeError(head_dir + " does not exist!")
+
+    fig_dir  = head_dir + '/results_figs'
+    if not os.path.isdir(fig_dir):
+        os.mkdir(fig_dir)
+        
+    poly_dir = head_dir + '/' + biomass
+    if not os.path.isdir(poly_dir):
+        raise RuntimeError(poly_dir + " does not exist!")
+        
+    if inp_type == 'melts':
+        poly_dir = poly_dir + '/pdi_' + str(dispval)
+        if not os.path.isdir(poly_dir):
+            raise RuntimeError(poly_dir + " does not exist!")
+
+    rundir = poly_dir + '/run_' + str(casenum)
+    if not os.path.isdir(rundir):
+        raise RuntimeError(rundir + " does not exist!")
+
+    if inp_type == 'solvents':
+        workdir1 = rundir + '/' + solv_type
+    elif inp_type == 'cosolvents':
+        h_workdir = rundir + '/' + solv_type
+        workdir1 = h_workdir + '/' + solv_type + '_water'
+    else:
+        workdir1 = rundir #even for inp_type = melts
+
+    if not os.path.isdir(workdir1):
+        raise RuntimeError(workdir1, " does not exist!")
+
+    anadir = workdir1 + '/' + subdir
+    if not os.path.isdir(workdir1):
+        raise RuntimeError(anadir, " does not exist!")
+
+    return workdir1, anadir, fig_dir
+#------------------------------------------------------------------        
+# Change width of seaborn barplot
+def change_width(ax, new_value):
+    for patch in ax.patches:
+        current_width = patch.get_width()
+        diff = current_width - new_value
+
+        # Change the bar width
+        patch.set_width(new_value)
+
+        # Recenter the bar
+        patch.set_x(patch.get_x() + diff * .5)
+#------------------------------------------------------------------
+# Return Chain ID from file name
+def ret_chid(inpfyle):
+    outstr = re.split(r'/|.xvg|_',inpfyle)
+    chid = outstr[len(outstr)-2]
+    if not chid.isdigit():
+        print('ERR: not a numeric value', chid)
+    else:
+        return int(chid)
+#------------------------------------------------------------------
+# Return the monomers in a chain
+def ret_mons(inpfyle):
+    if not os.path.exists(inpfyle):
+        raise RuntimeError('File not found ' + inpfyle)
+    mon_arr = []
+    with open(inpfyle) as finp:
+        finp.readline()
+        for line in finp:
+            line = line.rstrip('/n')
+            outstr = line.split()
+            mon_arr.append(int(outstr[len(outstr)-2]))
+    return mon_arr
+#------------------------------------------------------------------
+# All Rg plots
+def plot_allrg(df,fig_dir,pdi_val):
+    #Plot Rg^2 - T
+    figa, axa = plt.subplots()
+    axa.scatter(x=df['Temp'],y=df['<Rg^2>'])
+    set_axes(axa,plt,r'Temperature ($K$)',r'$R_{g}^{2}$ ($nm^{2}$)') 
+    figa.savefig(fig_dir + '/'+'rg2_' + str(pdi_val) + '.png',\
+                 dpi=figa.dpi)
+    plt.close(figa)
+    
+    #Plot Rg^4 - T
+    figa, axa = plt.subplots()
+    axa.scatter(x=df['Temp'],y=df['<Rg^4>'])
+    set_axes(axa,plt,r'Temperature ($K$)',r'$R_{g}^{4}$ ($nm^{4}$)') 
+    figa.savefig(fig_dir + '/'+'rg4_' + str(pdi_val) + '.png',\
+                 dpi=figa.dpi)
+    plt.close(figa)
+    
+    #Plot Alpha - T
+    figa, axa = plt.subplots()
+    axa.scatter(x=df['Temp'],y=df['Rg4/Rg2^2'])
+    set_axes(axa,plt,r'Temperature ($K$)',\
+             r'$\langle R_{g}^{4} \rangle/ \langle R_{g}^{2} \rangle^{2}$') 
+    figa.savefig(fig_dir + '/'+'alpha' + str(pdi_val) + '.png',\
+                 dpi=figa.dpi)
+    plt.close(figa)
+#------------------------------------------------------------------
+# Plot Tg
+def plot_tg(df,fig_dir,pdi_val):
+    figa, axa = plt.subplots()
+    set_axes(axa,plt,r'Temperature ($K$)',r'Specific Volume ($cm^3/g$)')
+    axa.scatter(x=df['Temp'],y=df['SV_NPT'])
+    figa.savefig(fig_dir + '/'+'sv_' + str(pdi_val) + '.png',\
+                 dpi=figa.dpi)
+    return figa, axa
+#------------------------------------------------------------------
+# All shape plots
+def plot_allshape(df,fig_dir,pdi_val):
+    #Plot lam_x - T
+    figa, axa = plt.subplots()
+    axa.scatter(x=df['Temp'],y=df['<lam_1>'])
+    set_axes(axa,plt,r'Temperature ($K$)',r'$\lambda_{x}$ ($nm^{2}$)') 
+    figa.savefig(fig_dir + '/'+'lamx_' + str(pdi_val) + '.png',\
+                 dpi=figa.dpi)
+    plt.close(figa)
+
+    #Plot lam_y - T
+    figa, axa = plt.subplots()
+    axa.scatter(x=df['Temp'],y=df['<lam_2>'])
+    set_axes(axa,plt,r'Temperature ($K$)',r'$\lambda_{y}$ ($nm^{2}$)') 
+    figa.savefig(fig_dir + '/'+'lamy_' + str(pdi_val) + '.png',\
+                 dpi=figa.dpi)
+    plt.close(figa)
+
+    #Plot lam_z - T
+    figa, axa = plt.subplots()
+    axa.scatter(x=df['Temp'],y=df['<lam_3>'])
+    set_axes(axa,plt,r'Temperature ($K$)',r'$\lambda_{z}$ ($nm^{2}$)') 
+    figa.savefig(fig_dir + '/'+'lamz_' + str(pdi_val) + '.png',\
+                 dpi=figa.dpi)
+    plt.close(figa)
+
+    #Plot kappa - T
+    figa, axa = plt.subplots()
+    axa.scatter(x=df['Temp'],y=df['<\kappa>'])
+    set_axes(axa,plt,r'Temperature ($K$)',r'$\kappa$ ($nm^{4}$)') 
+    figa.savefig(fig_dir + '/'+'kappa_' + str(pdi_val) + '.png',\
+                 dpi=figa.dpi)
+    plt.close(figa)
+#------------------------------------------------------------------
+# Plot b-nu data
+def plot_rgscaling(df,fig_dir,pdi_val):
+    #Plot b - T
+    figa, axa = plt.subplots()
+    axa.scatter(x=df['Temp'],y=df['b'])
+    set_axes(axa,plt,r'Temperature ($K$)',r'$C_{l}$ ($nm$)') 
+    figa.savefig(fig_dir + '/'+'lamx_' + str(pdi_val) + '.png',\
+                 dpi=figa.dpi)
+    plt.close(figa)
+
+    #Plot nu-T
+    figa, axa = plt.subplots()
+    axa.scatter(x=df['Temp'],y=df['nu'])
+    set_axes(axa,plt,r'Temperature ($K$)',r'$\nu$') 
+    figa.savefig(fig_dir + '/'+'lamx_' + str(pdi_val) + '.png',\
+                 dpi=figa.dpi)
+    plt.close(figa)
+#------------------------------------------------------------------
+# All shape plots
+def plot_allhb(df,fig_dir,pdi_val):
+    #Plot intra HB - T
+    figa, axa = plt.subplots()
+    axa.scatter(x=df['Temp'],y=df['Intra_HB'])
+    set_axes(axa,plt,r'Temperature ($K$)',r'#Intra HB') 
+    figa.savefig(fig_dir + '/'+'hbintra_' + str(pdi_val) + '.png',\
+                 dpi=figa.dpi)
+    plt.close(figa)
+
+    #Plot inter HB - T
+    figa, axa = plt.subplots()
+    axa.scatter(x=df['Temp'],y=df['Inter_HB'])
+    set_axes(axa,plt,r'Temperature ($K$)',r'Inter_HB') 
+    figa.savefig(fig_dir + '/'+'hbinter_' + str(pdi_val) + '.png',\
+                 dpi=figa.dpi)
+    plt.close(figa)
+
+    #Plot tot HB - T
+    figa, axa = plt.subplots()
+    axa.scatter(x=df['Temp'],y=df['Tot_HB'])
+    set_axes(axa,plt,r'Temperature ($K$)',r'#HB') 
+    figa.savefig(fig_dir + '/'+'hbtot_' + str(pdi_val) + '.png',\
+                 dpi=figa.dpi)
+    plt.close(figa)
+#------------------------------------------------------------------    
+# Plot axis details
+def set_axes(axhdl,plt,xlabel,ylabel):
+    plt.style.use('seaborn-colorblind')
+    plt.tight_layout()
+    axhdl.set_xlabel(xlabel)
+    axhdl.set_ylabel(ylabel)
+    change_width(axhdl,0.2)
+#------------------------------------------------------------------
+# find axes limits
+def axlims(yminref,ymin,ymaxref,ymax):
+    if ymax > ymaxref: ymaxref = ymax
+    if ymin < yminref: yminref = ymin
+    return yminref, ymaxref
+#------------------------------------------------------------------
+# Submit density job script
+def submit_dens(currdir,workdir,f_name,tmin,tmax,tdt):
+    os.chdir(workdir)
+    rev_fname = f_name.replace('_pyinp','')
+    fr  = open(workdir + '/' + f_name,'r')
+    fw  = open(workdir + '/' + rev_fname,'w')
+    fid = fr.read().replace("py_Tinit",str(tmin)).\
+          replace("py_Tfin",str(tmax)).\
+          replace("py_dT",str(tdt)).\
+          replace("py_jobname","denscomp")
+    fw.write(fid)
+    fw.close()
+    subprocess.call(["sbatch",rev_fname])
+    os.chdir(currdir)
+#------------------------------------------------------------------     
+def set_axes(axhdl,plt,xlabel,ylabel):
+    plt.style.use('seaborn-colorblind')
+    plt.tight_layout()
+    axhdl.set_xlabel(xlabel)
+    axhdl.set_ylabel(ylabel)
+    change_width(axhdl,0.2)
+#------------------------------------------------------------------     
+# if __name__
+if __name__ == '__main__':
+    main()
+#------------------------------------------------------------------
---
+++ src_matpy/plot_all.py
@@ -0,0 +1,115 @@
+# Plot all data
+# Version: Mar-04-2025
+# Import modules
+import os
+import math
+import analyze_rdfs as ardf
+#from compute_props import *
+from aux_plot import *
+from env_plot import *
+
+#---------------Directory paths-------------------------------------
+main_dir = os.getcwd() # current dir
+
+all_dir  = '../../new_results' # analysis dir
+if not os.path.isdir(all_dir):
+    print("FATAL ERROR: ", all_dir, " not found")
+    exit("Check scratch directory path")
+    
+anaout_dir = '../../analyzed_results'
+if not os.path.isdir(anaout_dir):
+    os.mkdir(anaout_dir)
+    
+figout_dir = '../../figures'
+if not os.path.isdir(figout_dir):
+    os.mkdir(figout_dir)  
+
+#---------Input Data-----------------------------------------------
+syspref  = '_qanion_'
+respref  = 'allresults_'
+q_anion  = '0.2' # should be the exact string value
+sys_arr  = ['S1','S2','S3']
+fran_arr = ['0.05','0.09','0.20']
+f_unpoly = [0, 0.6, 0.6]
+deltat   = [0.006, 0.006, 0.006] 
+case_arr = [1]
+molwt    = 40
+nchains  = 100
+rho_tot  = 0.8
+
+#---------File prefixes------------------------------------------
+rdf_pref     = 'rdf_config_'
+rg_pref      = 'rgavg_config_'
+neigh_pref   = 'catanneigh_config_'
+restime_pref = ['autocorrcion_config','autocorrpol_config_']
+diff_pref    = ['iondiff_config_', 'countiondiff_config_']
+clust_pref   = 'clust_config_'
+bfrdf_pref   = 'freeboundrdf_config_'
+
+#---------Flags---------------------------------------------------
+rdf_flag     = 1;
+rdf_keys     = {'LiAn':2,'LiPoly':3}
+rg_flag      = 0
+neigh_flag   = 0
+restime_flag = 0
+diff_flag    = 0
+clust_flag   = 0
+bfrdf_flag   = 0
+
+#---------Calculations----------------------------------------------
+nanions  = [nchains*int(molwt*float(x)) for x in fran_arr]
+npols    = [nchains*molwt - nanions[i]  for i in range(len(nanions))]
+totbeads = [int(2.0*(npols[i] + nanions[i])) for i in range(len(npols))]
+npol_ans = [int(0.5*float(x)) for x in npols]
+frho_an  = [rho_tot*(nanions[i]/totbeads[i]) for i in range(len(totbeads))]
+frho_pol = [rho_tot*(npol_ans[i]/totbeads[i]) for i in range(len(totbeads))]  
+
+#-----Sanity checks------------------------------------------------
+if len(fran_arr) != len(deltat):
+    raise RuntimeError('Mismatch in lengths between delta_t and fran_arr')
+
+#-------Main analysis----------------------------------------------
+for sysid,sysname in enumerate(sys_arr):
+    
+    sys_dir = all_dir + '/' + sysname + syspref + q_anion
+    if not os.path.isdir(sys_dir):
+        raise RuntimeError("FATAL ERROR: " + sys_dir + " not found")
+
+    for casenum in case_arr:
+               
+        if(rdf_flag):
+                
+            for cid, (rdfkey,colval) in enumerate(rdf_keys.items()):
+
+                # Plot data
+                fig, ax = plt.subplots()
+                set_axes(ax,plt,r'$r$ ($\sigma$)',r'g(r), n(r)')
+
+                for fr_id, frac_an in enumerate(fran_arr):
+
+                    if not 'Poly' in rdfkey:
+                        rhof = frho_an[fr_id]
+                    else:
+                        rhof = frho_pol[fr_id]*(1-f_unpoly[sysid])
+
+                    result_dir = sys_dir + '/' + respref + sysname + '_' + frac_an + \
+                        '_' + str(casenum)
+
+                    if not os.path.isdir(result_dir):
+                        raise RuntimeError("FATAL ERROR: " + result_dir + " not found")
+
+                    print ("RDF analysis for ", result_dir)
+                    rarr,gofr,nofr = ardf.ana_rdf(result_dir,rdf_pref,colval-1)
+                    plt.plot(rarr, gofr, color = clr_arr[fr_id], linestyle = '--', \
+                             label = '$f_{an}$ = ' + str(frac_an))
+                    plt.plot(rarr, rhof*nofr, color = clr_arr[fr_id], linestyle='-')
+
+                ax.set_xlim([0, 3])
+                ax.legend()
+                fig.savefig(f'{figout_dir}/rdf_{sysname}_{rdfkey}.png',dpi = fig.dpi)
+                fig.savefig(f'{figout_dir}/rdf_{sysname}_{rdfkey}.eps',format = 'eps')
+
+
+            
+        
+
